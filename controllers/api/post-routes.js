466cd403d23009ecd1d7eceb0e3153f941ea94c3
unknown <nick_clymer2018@yahoo.com>
2021-07-10 17:09:42 -0600

    spacing cleanup

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -3,7 +3,6 @@ const { Post, User, Comment } = require('../../models');
 const sequelize = require('../../config/connection');
 const withAuth = require('../../utils/auth');
 
-
 router.get('/', (req, res) => {
     console.log('Fetching posts');
     Post.findAll({
