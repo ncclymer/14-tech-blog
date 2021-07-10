6b73d63ff073a43c8e3d8d13533eed8c4ecaec09
unknown <nick_clymer2018@yahoo.com>
2021-07-10 16:11:49 -0600

    added dependencies to package.json and express server

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const routes = require('./controllers');
+const sequelize = require('./config/connection');
+const path = require('path');
+const helpers = require('.utils/helpers');
+const exhndlbrs = require('express-handlebars');
+const hndlbrs = exhndlbrs.create({ helpers });
+const session = require('express-session');
+const app = express();
+const PORT = process.env.PORT || 3002;
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const newSession = {
+    secret: 'whoknows',
+    cookie: {
+        expires: 60 * 60 *1000
+    },
+    resave: true,
+    rolling: true,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    }),
+};
+
+app.use(session(newSession));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.engine('handlebars', hndlbrs.engine);
+app.set('view engine', 'handlebars');
+app.use(routes);
+
+sequelize.sync({force: false }).then(() => {
+    app.listen(PORT, () => console.log('Now listening on port 3002.'));
+});
