d7066a443372dfe9b8ecf74be3ca54495b7d71c9
unknown <nick_clymer2018@yahoo.com>
2021-07-12 15:39:09 -0600

    bug fixes

--- public/js/delete-post.js
+++ public/js/delete-post.js
@@ -1,25 +1,26 @@
 async function deleteFormHandler(event) {
     event.preventDefault();
-
+    
     const id = window.location.toString().split('/')[
-        window.location.toString90.split('/').length - 1
-    ];
+        window.location.toString().split('/').length - 1
+      ];
 
-    const response = await fetch(`api/posts/${id}`, {
+    const response = await fetch(`/api/posts/${id}`, {
         method: 'DELETE',
         body: JSON.stringify({
-            post_id: id
+          post_id: id
         }),
         headers: {
-            'Content-Type': 'application.json'
+          'Content-Type': 'application/json'
         }
-    });
-
-    if (response.ok) {
+      });
+      
+      if (response.ok) {
         document.location.replace('/dashboard/');
-    } else {
+      } else {
         alert(response.statusText);
-    }
-}
-
-document.querySelector('.delete-post-btn').addEventListener('click', deleteFormHandler);
+      }
+    
+  }
+  
+  document.querySelector('.delete-post-btn').addEventListener('click', deleteFormHandler);
