ccfd364a026d6808638ec10fdecefa9bbf36af43
unknown <nick_clymer2018@yahoo.com>
2021-07-13 12:40:38 -0600

    heroku deployment config key changes

--- config/connection.js
+++ config/connection.js
@@ -5,8 +5,8 @@ require('dotenv').config();
 
 //db connection
 let sequelize;
-if (process.env.JAWSDB_WHITE_URL) {
-    sequelize = new Sequelize(process.env.JAWSDB_WHITE_URL);
+if (process.env.JAWSDB) {
+    sequelize = new Sequelize(process.env.JAWSDB);
 } else {
     sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
         host: 'localhost',
