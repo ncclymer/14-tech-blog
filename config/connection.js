df1edbb1d58409e9c917f9d077603df9f943a475
unknown <nick_clymer2018@yahoo.com>
2021-07-13 12:19:27 -0600

    connecting jawsdb to heroku deployment

--- config/connection.js
+++ config/connection.js
@@ -6,7 +6,7 @@ require('dotenv').config();
 //db connection
 let sequelize;
 if (process.env.JAWSDB_URL) {
-    sequelize = new Sequelize(process.env.JAWSDB_URL);
+    sequelize = new Sequelize(process.env.JAWSDB_WHITE_URL);
 } else {
     sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
         host: 'localhost',
