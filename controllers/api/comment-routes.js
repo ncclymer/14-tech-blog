c34798ae0c75cfe6957b5546c91e472d9a71d77a
unknown <nick_clymer2018@yahoo.com>
2021-07-12 18:13:06 -0600

    final known bug fixes

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -4,44 +4,46 @@ const withAuth = require('../../utils/auth');
 
 router.get('/', (req, res) => {
     Comment.findAll({})
-        .then(dbComment => res.json(dbComment))
-        .catch(err => {
-            console.log(err);
-            res.status(500).json(err);
-        });
+      .then(dbCommentData => res.json(dbCommentData))
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
 });
 
-router.post('./', withAuth, (req, res) => {
-    if (req.session) {
-        Comment.create({
-            comment_text: req.body.comment_text,
-            post_id: req.session.post_id,
-            user_id: req.session.user_id
-        })
-            .then(dbComment => res.json(dbComment))
-            .catch(err => {
-                console.log(err);
-                res.status(400).json(err);
-            });
-    }
+router.post('/', withAuth, (req, res) => {
+
+  if (req.session) {
+    Comment.create({
+      comment_text: req.body.comment_text,
+      post_id: req.body.post_id,
+    
+      user_id: req.session.user_id,
+    })
+      .then(dbCommentData => res.json(dbCommentData))
+      .catch(err => {
+        console.log(err);
+        res.status(400).json(err);
+      });
+  }
 });
 
 router.delete('/:id', withAuth, (req, res) => {
     Comment.destroy({
         where: {
-            id: req.params.id
+          id: req.params.id
         }
-    })
-        .then(dbComment => {
-            if (!dbComment) {
-                res.status(404).json({ message: "Couldn't find a post with that id." });
-                return;
-            }
-            res.json(dbComment);
+      })
+        .then(dbCommentData => {
+          if (!dbCommentData) {
+            res.status(404).json({ message: 'No comment found with this id' });
+            return;
+          }
+          res.json(dbCommentData);
         })
         .catch(err => {
-            console.log(err);
-            res.status(500).json(err);
+          console.log(err);
+          res.status(500).json(err);
         });
 });
 
